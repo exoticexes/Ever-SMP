971762ffd165f76dd5a1a1686e620287511e5dd1
exoticexes <mertrobloxtr@gmail.com>
2026-02-13 23:08:14 +0300

    index

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const mineflayer = require('mineflayer')
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'play.everyones.run.place', // Senin görselindeki IP
-        port: 24107,                  // Senin görselindeki Port
-        username: 'Kivi_Nöbette',      // Botun oyundaki adı
-        version: '1.21.1'             // Sunucu sürümün (Değilse değiştir)
+        host: 'play.everyones.run.place', // Yeni verdiğin IP adresi
+        port: 24107,                     // Portun (Eğer bu da değiştiyse panelden kontrol et)
+        username: 'Kivi_Nöbette',        // Botun ismi
+        version: '1.21.11'               // Loglarda görünen doğru sürüm
     })
 
     bot.on('spawn', () => {
-        console.log('Bot başarıyla bağlandı! Sunucu aktif tutuluyor.');
+        console.log('Bot sunucuya girdi! Kivi su an nöbette.');
     });
 
-    // AFK atılmamak için 1 dakikada bir zıplama
+    // AFK atılmayı önlemek için zıplama
     setInterval(() => {
         if (bot.entity) {
             bot.setControlState('jump', true);
@@ -21,11 +21,11 @@ function createBot() {
     }, 60000);
 
     bot.on('end', () => {
-        console.log('Bağlantı koptu, 10 saniye sonra tekrar bağlanacak...');
+        console.log('Bağlantı koptu, 10 saniye sonra tekrar denenecek...');
         setTimeout(createBot, 10000);
     });
 
-    bot.on('error', (err) => console.log('Hata oluştu:', err));
+    bot.on('error', (err) => console.log('Hata:', err));
 }
 
 createBot();
