4c433266eefca88b397f8a8e298bee2c29015b50
exoticexes <mertrobloxtr@gmail.com>
2026-02-13 22:48:43 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'everyoneesmp.falix.gg', // Senin görselindeki IP
+        port: 24107,                  // Senin görselindeki Port
+        username: 'Kivi_Nöbette',      // Botun oyundaki adı
+        version: '1.20.1'             // Sunucu sürümün (Değilse değiştir)
+    })
+
+    bot.on('spawn', () => {
+        console.log('Bot başarıyla bağlandı! Sunucu aktif tutuluyor.');
+    });
+
+    // AFK atılmamak için 1 dakikada bir zıplama
+    setInterval(() => {
+        if (bot.entity) {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }
+    }, 60000);
+
+    bot.on('end', () => {
+        console.log('Bağlantı koptu, 10 saniye sonra tekrar bağlanacak...');
+        setTimeout(createBot, 10000);
+    });
+
+    bot.on('error', (err) => console.log('Hata oluştu:', err));
+}
+
+createBot();
