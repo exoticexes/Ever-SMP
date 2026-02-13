5fd00a9cb95158450c67456c1440703535e585b0
exoticexes <mertrobloxtr@gmail.com>
2026-02-13 23:17:33 +0300

    update js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const mineflayer = require('mineflayer')
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'play.everyones.run.place', // Yeni verdiğin IP
-        port: 24107,                     // Panelindeki Port
+        host: 'play.everyones.run.place', // Senin verdiğin IP
+        port: 24107,                     // Görseldeki portun
         username: 'Kivi_724',            
-        version: '1.21.1'                // Hataya sebep olan sürüm burada düzeltildi
+        version: '1.21.11'               // Logdaki hatayı çözen tam sürüm
     })
 
-    bot.on('spawn', () => console.log('BOT OYUNDA! Sunucu artık 7/24.'));
+    bot.on('spawn', () => console.log('BAŞARILI: Bot sunucuda!'));
 
     setInterval(() => {
         if (bot.entity) bot.setControlState('jump', true), setTimeout(() => bot.setControlState('jump', false), 500);
     }, 60000);
 
     bot.on('end', () => setTimeout(createBot, 5000));
-    bot.on('error', (err) => console.log('Hata:', err.message));
+    bot.on('error', (err) => console.log('Hata detayı:', err.message));
 }
 
 createBot();
